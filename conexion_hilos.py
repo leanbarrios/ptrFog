6a769b84bac7ce7261a2cab7e7f0d012cec960dc
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2017-05-23 17:01:48 -0300

    Ejemplo de hilos de ejecucion

--- conexion_hilos.py
+++ conexion_hilos.py
@@ -1,6 +1,11 @@
-		
-def conexion():
-	print("Sin conexion")
+import threading
 
-
-conexion()
+def trabajo():
+    """funcion que realiza el trabajo en el thread"""
+    print 'Este es un hilo - Tomas se la come'
+    return
+threads = list()
+for i in range(3):
+    t = threading.Thread(target=trabajo)
+    threads.append(t)
+    t.start()
