15f2b0225e13a157243f60ce38425b92edee46f1
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2017-05-24 16:20:01 -0300

    s

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ class VentanaMonitor(Ventana):
 		Ventana.__init__(self,"600","800","Monitor de objetos")
 	
 	def elementos(self,ventana):
-		etiqueta = Label(ventana,text="Maxi prueba")
-		etiqueta.pack()
+		etiqueta01 = Label(ventana,text="Objeto 1: ").grid(row=0,column=0)
+		etiqueta02 = Label(ventana,text="Objeto 2: ").grid(row=1,column=0)
+		etiqueta03 = Label(ventana,text="Objeto 3: ").grid(row=2,column=0)
+		etiqueta04 = Label(ventana,text="Objeto 4: ").grid(row=3,column=0)
+		etiqueta01 = Label(ventana,text="Auto policial: ").grid(row=0,column=1)
+		etiqueta02 = Label(ventana,text="Auto generico: ").grid(row=1,column=1)
+		etiqueta03 = Label(ventana,text="Error: ").grid(row=2,column=1)
+		etiqueta04 = Label(ventana,text="Semaforo: ").grid(row=3,column=1)
 	
 
 def main():
